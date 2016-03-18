5c707a2809d0c60cf780a2d00667ada8714438c8
Sagar Anand <sagar.anand015@gmail.com>
2016-03-18 16:47:50 +0530

    modified 3rd theme to remove the loading screen

--- 3/js/main.js
+++ 3/js/main.js
@@ -11,11 +11,11 @@
    ------------------------------------------------------ */ 
   	$(window).load(function() {
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+   	 //  // will first fade out the loading animation 
+    	// $("#status").fadeOut("slow"); 
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();     
+    	// // will fade out the whole DIV that covers the website. 
+    	// $("#preloader").delay(500).fadeOut("slow").remove();     
       
     	$('.js #hero .hero-image img').addClass("animated fadeInUpBig"); 
       $('.js #hero .buttons a.trial').addClass("animated shake");    
